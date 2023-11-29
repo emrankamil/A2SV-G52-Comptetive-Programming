0212152122ccfda0407ccd655e76fabb6a6ee284
Emran Kamil <128908090+emrankamil@users.noreply.github.com>
2023-11-29 19:06:18 +0300

    Added solution for Palindrome Number

---
+++ week-1/palindrome-number.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if 0 <= x < 10:
+            return True
+        if x < 0 or x % 10 == 0:
+            return False
+
+
+        reversed_half = 0
+        while x > reversed_half:
+            reversed_half = reversed_half * 10 + x % 10
+            x //= 10
+
+        return x == reversed_half or x == reversed_half // 10
