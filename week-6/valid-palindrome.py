80f223e31b04ab5ef7961550411f5a82567bd74f
Emran Kamil <128908090+emrankamil@users.noreply.github.com>
2024-01-03 17:46:27 +0300

    Added solution for Valid Palindrome

--- week-6/valid-palindrome.py
+++ week-6/valid-palindrome.py
@@ -1,17 +1,17 @@
-class Solution(object):
-    def isPalindrome(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-        
-        alphanumeric=[]
-        for i in s:
-            if i.isalnum():
-                alphanumeric.append(i.lower())
-        n=len(alphanumeric)
-        for j in range(n):
-            if alphanumeric[j]!=alphanumeric[n-j-1]:
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        left = 0
+        right = len(s)-1
+
+        while right > left:
+            if not s[left].isalnum():
+                left += 1
+            elif not s[right].isalnum():
+                right -= 1
+            elif s[right].lower() != s[left].lower():
                 return False
+            else:
+                left += 1
+                right -= 1
         return True
-        
+            
