35cec797ef5b293819e854266afdbb29d6b6ea0a
Emran Kamil <128908090+emrankamil@users.noreply.github.com>
2024-01-13 13:24:59 +0300

    Added solution for 3Sum

---
+++ week-7/3sum.py
@@ -0,0 +1,42 @@
+class Solution(object):
+    def threeSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        nums.sort()  # Sort the array in ascending order
+        triplets = []
+
+        for i in range(len(nums)-2):
+            # Skip duplicates of the fixed element
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+
+            left = i + 1
+            right = len(nums) - 1
+
+            while left < right:
+                total = nums[i] + nums[left] + nums[right]
+
+                if total < 0:
+                    left += 1
+                elif total > 0:
+                    right -= 1
+                else:
+                    # Found a triplet with sum 0
+                    triplet = [nums[i], nums[left], nums[right]]
+                    triplets.append(triplet)
+
+                    # Skip duplicates of the other two elements
+                    while left < right and nums[left] == nums[left+1]:
+                        left += 1
+                    while left < right and nums[right] == nums[right-1]:
+                        right -= 1
+
+                    left += 1
+                    right -= 1
+
+        return triplets
+
+
+    
