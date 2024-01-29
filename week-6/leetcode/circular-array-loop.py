0140372d491cea209b9e5bd59bdb21d47ef9333b
Emran Kamil <128908090+emrankamil@users.noreply.github.com>
2024-01-29 10:14:06 +0300

    Add solution for Circular Array Loop

--- week-6/leetcode/circular-array-loop.py
+++ week-6/leetcode/circular-array-loop.py
@@ -1,35 +1,5 @@
 class Solution:
     def circularArrayLoop(self, nums: List[int]) -> bool:
-        checkSet = set()
-        slow, fast, n = 0, 0, len(nums)
-        check = False
-
-        while fast * nums[fast] >= 0:
-            if check:
-                slow = (slow+nums[slow])%n
-                check = not check
-            fast = (fast+nums[fast])%n
-            
-            if slow == fast:
-                # fast = (fast+nums[fast])%n
-                # while slow != fast:
-                #     if slow*fast < 0:
-                #         return False
-                #     fast = (fast+nums[fast])%n
-                return True
-            x = (slow, fast)
-            if x in checkSet:
-                return False
-            else:
-                checkSet.add(x)
-       
-        return False
-
-# here we are using the array it self when we jump from one 
-# index to the other
-
-class Solution:
-    def circularArrayLoop(self, nums: List[int]) -> bool:
 
         n = len(nums)
 
