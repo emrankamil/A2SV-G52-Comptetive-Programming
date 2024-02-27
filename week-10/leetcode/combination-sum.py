41de744afa5026f479462819382d03a5c588b10c
Emran Kamil <128908090+emrankamil@users.noreply.github.com>
2024-02-27 10:23:47 +0300

    Add solution for Combination Sum

--- week-10/leetcode/combination-sum.py
+++ week-10/leetcode/combination-sum.py
@@ -1,25 +1,15 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        sum = 0
         rst = []
-        subArray = []
+        def backtrack(start, cur, target):
+            if target == 0:
+                rst.append(cur[:])
 
-        def dfs(sum, subArray, cand):
-            nonlocal rst
-            if sum == target:
-                rst.append(subArray.copy())
-                return 
-            elif sum > target or len(cand)==0:
-                return 
-            else:
-                for i in range(len(cand)):
-                    sum += cand[i]
-                    subArray.append(cand[i])
-                    dfs(sum,subArray,cand[i:])
-                    sum -= cand[i]
-                    subArray.pop()
+            for i in range(start, len(candidates)):
+                if candidates[i] <= target:
+                    cur.append(candidates[i])
+                    backtrack(i, cur, target-candidates[i])
+                    cur.pop()
         
-        dfs(0,subArray,candidates)
+        backtrack(0, [], target)
         return rst
-
-
