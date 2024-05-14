dbc29ed663ea452913242e21fc8fa8dcc175d3b6
Emran Kamil <128908090+emrankamil@users.noreply.github.com>
2024-05-14 18:58:00 +0300

    Created 'D - Meron and Games' by A2SV Hub

---
+++ A2SV G5 - Contest #16 14-May-2024/D - Meron and Games 149971.py
@@ -0,0 +1,36 @@
+# Problem: D - Meron and Games - https://codeforces.com/gym/523525/problem/D
+
+
+from collections import Counter, defaultdict
+import sys, threading
+ 
+def main():
+    n = int(input())
+    l = list(map(int, input().split()))
+    c = Counter(l)
+    lst = list(set(l))
+    lst.sort()
+
+    memo = defaultdict(int)
+    def dp(i):
+        if i >= len(lst):
+            return 0
+        if i not in memo:
+            rst = 0
+            if i < len(lst)-1 and lst[i+1] == lst[i]+1:
+                pick = lst[i] * c[lst[i]] + dp(i+2)
+            else:
+                pick = lst[i] * c[lst[i]] + dp(i+1)
+            rst = max(pick , dp(i+1))
+            memo[i] = rst
+        return memo[i]
+
+    print(dp(0))
+ 
+    
+sys.setrecursionlimit(1 << 30)
+threading.stack_size(1 << 27)
+ 
+main_thread = threading.Thread(target=main)
+main_thread.start()
+main_thread.join()
