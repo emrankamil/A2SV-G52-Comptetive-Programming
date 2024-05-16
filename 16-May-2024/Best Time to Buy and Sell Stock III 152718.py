8e3efc0927bbe72325dcf2ac3b8979bb86c38cf9
Emran Kamil <128908090+emrankamil@users.noreply.github.com>
2024-05-16 23:53:28 +0300

    Created 'Best Time to Buy and Sell Stock III' by A2SV Hub

---
+++ 16-May-2024/Best Time to Buy and Sell Stock III 152718.py
@@ -0,0 +1,13 @@
+# Problem: Best Time to Buy and Sell Stock III - https://leetcode.com/problems/best-time-to-buy-and-sell-stock-iii/
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        buy1, buy2 = -prices[0], -prices[0]
+        sell1, sell2 = 0, 0
+        
+        for price in prices:
+            buy1 = max(buy1, -price)
+            sell1 = max(sell1, buy1 + price)
+            buy2 = max(buy2, sell1 -price)
+            sell2 = max(sell2, buy2 + price)
+        return sell2
