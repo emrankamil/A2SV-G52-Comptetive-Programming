ae03c41e6ab71139e3bef5922ff1234902b7e080
Emran Kamil <128908090+emrankamil@users.noreply.github.com>
2024-05-25 17:42:16 +0300

    Created 'Combination Sum IV' by A2SV Hub

---
+++ 25-May-2024/Combination Sum IV 159325.py
@@ -0,0 +1,12 @@
+# Problem: Combination Sum IV - https://leetcode.com/problems/combination-sum-iv/description/
+
+class Solution:
+    def combinationSum4(self, nums: List[int], target: int) -> int:
+        dp = [0] * (target + 1)
+        dp[0] = 1
+        for amount in range(1, target + 1):
+            for num in nums:
+                if amount - num >= 0:
+                    dp[amount] += dp[amount - num]
+        
+        return dp[-1]
