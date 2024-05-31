736c98b9f4f23745775b5e0311170eab001355d5
Emran Kamil <128908090+emrankamil@users.noreply.github.com>
2024-05-31 18:45:25 +0300

    Created 'A - Ascend' by A2SV Hub

---
+++ A2SV G5 - Contest #18 31-May-2024/A - Ascend 162755.py
@@ -0,0 +1,13 @@
+# Problem: A - Ascend - https://codeforces.com/gym/526229/problem/A
+
+n = int(input())
+l = list(map(int, input().split()))
+rst = 1
+cur = 1
+for i in range(1, n):
+    if l[i] > l[i-1]:
+        cur += 1
+    else:
+        rst = max(rst, cur)
+        cur = 1
+print(max(rst, cur))
